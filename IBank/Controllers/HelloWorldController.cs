b5472c66ef283af3e876a90f5f7a44f7ece29d1b
Junior <jmota.wk@hotmail.com>
2021-12-30 10:51:54 -0300

    add: hello world

---
+++ IBank/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IBank.Controllers
+{
+    [ApiController]
+    [Route("hello-world")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello world!";
+        }
+    }
+}
