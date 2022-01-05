165f279da8b63dc131172cf05661607f6ad3b193
Junior <jmota.wk@hotmail.com>
2022-01-05 08:17:07 -0300

    add: post deployment table filling script.

---
+++ IBankDB/Script.PostDeployment.sql
@@ -0,0 +1,11 @@
+﻿IF NOT EXISTS (SELECT 1 FROM [dbo].[TransactionAction])
+BEGIN
+	INSERT INTO [dbo].TransactionAction(Name) 
+	VALUES ('Withdrawal'), ('Deposit'), ('Transference');
+END
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Agency])
+BEGIN
+	INSERT INTO [dbo].Agency(Number, Digit) 
+	VALUES ('0001', '0');
+END
