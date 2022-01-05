5de989d52e1148315b3bec185393619b23e2b68d
Junior <jmota.wk@hotmail.com>
2022-01-05 08:23:06 -0300

    update: auth + di setup.

--- IBank/Startup.cs
+++ IBank/Startup.cs
@@ -1,16 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DataAccess.Connection;
+using DataAccess.Data.Account;
+using DataAccess.Data.Agency;
+using DataAccess.Data.Client;
+using DataAccess.Data.Transaction;
+using IBank.Services.Account;
+using IBank.Services.Auth;
+using IBank.Services.Client;
+using IBank.Services.Token;
+using IBank.Services.Transaction;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace IBank
 {
@@ -31,7 +37,43 @@ namespace IBank
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "IBank", Version = "v1" });
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization using the Bearer scheme. Example: \"bearer {token}\"",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
             });
+
+            services.AddSingleton<IAccountService, AccountService>();
+            services.AddSingleton<IAuthService, AuthService>();
+            services.AddSingleton<IClientService, ClientService>();
+            services.AddSingleton<ITransactionService, TransactionService>();
+            services.AddSingleton<ITokenService, TokenService>();
+
+            services.AddSingleton<IAccountData, AccountData>();
+            services.AddSingleton<IAgencyData, AgencyData>();
+            services.AddSingleton<IClientData, ClientData>();
+            services.AddSingleton<IConnection, SqlConnection>();
+            services.AddSingleton<ITransactionData, TransactionData>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            System.Text.Encoding.UTF8.GetBytes(
+                                Configuration.GetSection("AppSettings:Jwt:Secret").Value
+                            )
+                        ),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +89,8 @@ namespace IBank
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
